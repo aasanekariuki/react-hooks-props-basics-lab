ee75281f000b5dbf442094bab04b9df720c84776
aasane kariuki <franciskariki053@gmail.com>
2024-04-27 12:28:53 +0300

    props-basics-lab done

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+
 console.log(user);
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+    
     </div>
   );
 }
